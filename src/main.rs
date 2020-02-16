da2d6d22fefcde88f605fc831bdf11fccf5d637b
yutiansut <yutiansut@qq.com>
2020-02-16 19:16:40 +0800

    first commit
    Signed-off-by: yutiansut <yutiansut@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,12 @@
-use mongodb::{Client, options::ClientOptions};
-use mongodb::options::FindOptions;
-use bson::{Bson, oid};
-
-use bson::Document;
-use bson::{doc, bson};
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct stock_day {
-
-    open : f64,
-    close : f64,
-    high : f64,
-    low : f64,
-    vol : f64,
-    amount : f64,
-    date : String,
-    code : String,
-//    date_stamp : f64
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct stock_min {
-    open : f64,
-    close : f64,
-    high : f64,
-    low : f64,
-    vol : f64,
-    amount : f64,
-    date : String,
-    datetime: String,
-    code : String,
-//    date_stamp : f64,
-//    time_stamp : f64,
-    #[serde(rename = "type")]
-    frequence: String,
-}
-
+use qadata_rs::qafetch::QAMongoClient;
+use qadata_rs::qautil::stock_min;
 
 fn main() {
-    // Parse a connection string into an options struct.
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").unwrap();
-
-
-    client_options.app_name = Some("QUANTAXIS".to_string());
-
-
-    let client = Client::with_options(client_options).unwrap();
-
-    let db = client.database("quantaxis");
-    let collection = db.collection("stock_min");
-
-    let filter = doc! {"code": "000001", "type": "5min", "datetime": {"$gte": "2020-01-02 09:00:00"}};
-    let find_options = FindOptions::builder().build();
-    let cursor = collection.find(filter, find_options).unwrap();
-
-    for result in cursor {
-        match result {
-            Ok(document) => {
-//                let x  = document.get("code").and_then(Bson::as_str);
-                let u:stock_min = bson::from_bson(bson::Bson::Document(document)).unwrap();
-                println!("code: {:#?}", u);
-
-            }
-            Err(e) => {println!("ERROR");} //return Err(e.into()),
-        }
-    }
+    let mut dataclient = QAMongoClient::new();
+    let stock_day = dataclient.get_stock_day("000001", "2020-01-01", "2020-02-01");
+    println!("{:#?}", stock_day);
+    let stock_min = dataclient.get_stock_min("000001", "2020-01-01", "2020-02-01", "5min");
+    println!("{:#?}", stock_min);
+    let future_min = dataclient.get_future_min("RBL8", "2020-01-01", "2020-02-01", "5min");
+    println!("{:#?}", future_min);
 }
---
+++ src/qafetch.rs
@@ -0,0 +1,110 @@
+use std::path::Prefix::Verbatim;
+
+use mongodb::{Client, options::ClientOptions};
+use mongodb::Database;
+use mongodb::options::FindOptions;
+
+use bson::{Bson, oid};
+use bson::{bson, doc};
+use bson::Document;
+
+use crate::qautil::{future_day, future_min, stock_day, stock_min};
+
+pub struct QAMongoClient {
+    uri: String,
+    database: Database,
+}
+
+impl QAMongoClient {
+    pub fn new() -> Self {
+        let uri = "mongodb://localhost:27017";
+        let mut client_options = ClientOptions::parse(uri).unwrap();
+        client_options.app_name = Some("QUANTAXIS".to_string());
+        let client = Client::with_options(client_options).unwrap();
+        let db = client.database("quantaxis");
+        Self {
+            uri: uri.to_string(),
+            database: db,
+        }
+    }
+
+    pub fn get_stock_day(&mut self, code: &str, start: &str, end: &str) -> Vec<stock_day> {
+        let collection = self.database.collection("stock_day");
+
+        let filter = doc! {"code": code,
+                                            "date": {"$gte": start, "$lte": end}};
+        let find_options = FindOptions::builder().build();
+        let cursor = collection.find(filter, find_options).unwrap();
+        let mut res = Vec::new();
+        for result in cursor {
+            match result {
+                Ok(document) => {
+                    let u: stock_day = bson::from_bson(bson::Bson::Document(document)).unwrap();
+                    res.push(u);
+                }
+                Err(e) => { println!("ERROR"); } //return Err(e.into()),
+            }
+        }
+        res
+    }
+
+    pub fn get_stock_min(&mut self, code: &str, start: &str, end: &str, frequence: &str) -> Vec<stock_min> {
+        let collection = self.database.collection("stock_min");
+
+        let filter = doc! {"code":code, "type": frequence, "datetime": {"$gte": start, "$lte": end}};
+        let find_options = FindOptions::builder().build();
+        let cursor = collection.find(filter, find_options).unwrap();
+
+        let mut res = Vec::new();
+        for result in cursor {
+            match result {
+                Ok(document) => {
+                    let u: stock_min = bson::from_bson(bson::Bson::Document(document)).unwrap();
+                    res.push(u);
+                }
+                Err(e) => { println!("ERROR"); } //return Err(e.into()),
+            }
+        }
+        res
+    }
+    pub fn get_future_day(&mut self, code: &str, start: &str, end: &str) -> Vec<future_day> {
+        let collection = self.database.collection("future_day");
+
+        let filter = doc! {"code": code,
+                                            "date": {"$gte": start, "$lte": end}};
+        let find_options = FindOptions::builder().build();
+        let cursor = collection.find(filter, find_options).unwrap();
+        let mut res = Vec::new();
+        for result in cursor {
+            match result {
+                Ok(document) => {
+                    let u: future_day = bson::from_bson(bson::Bson::Document(document)).unwrap();
+                    res.push(u);
+                }
+                Err(e) => { println!("ERROR"); } //return Err(e.into()),
+            }
+        }
+        res
+    }
+
+    pub fn get_future_min(&mut self, code: &str, start: &str, end: &str, frequence: &str) -> Vec<future_min> {
+        let collection = self.database.collection("future_min");
+
+        let filter = doc! {"code":code, "type": frequence, "datetime": {"$gte": start, "$lte": end}};
+        let find_options = FindOptions::builder().build();
+        let cursor = collection.find(filter, find_options).unwrap();
+
+        let mut res = Vec::new();
+        for result in cursor {
+            match result {
+                Ok(document) => {
+                    let u: future_min = bson::from_bson(bson::Bson::Document(document)).unwrap();
+                    res.push(u);
+                }
+                Err(e) => { println!("ERROR"); } //return Err(e.into()),
+            }
+        }
+        res
+    }
+}
+
