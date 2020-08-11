8419447209cd1e5014535ee58d5a8a4f45571969
yutiansut <yutiansut@qq.com>
2020-08-11 23:04:29 +0800

    update

--- src/qafetch.rs
+++ src/qafetch.rs
@@ -8,8 +8,17 @@ use bson::{Bson, oid};
 use bson::{bson, doc};
 use bson::Bson::Array;
 use bson::Document;
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
+
 
 use crate::qautil::{future_day, future_min, stock_day, stock_min};
+
+fn to_timestamp(date: String) -> i64{
+    Utc.datetime_from_str(format!("{} 00:00:00", date).as_str(), "%Y-%m-%d %H:%M:%S")
+    .unwrap()
+    .timestamp() -28800
+}
+
 
 pub struct QAMongoClient {
     pub uri: String,
@@ -29,9 +38,9 @@ impl QAMongoClient {
     }
     pub fn get_stocks_day(&mut self, code: Vec<String>, start: &str, end: &str) -> Vec<stock_day> {
         let collection = self.database.collection("stock_day");
-        println!("start {} end {}", start, end);
+        //println!("start {} end {}", start, end);
         let filter = doc! {"code": {"$in": code},
-                                            "date": {"$gte": start, "$lte": end}};
+                                            "date_stamp": {"$gte": to_timestamp(start.to_string()), "$lte":  to_timestamp(end.to_string())}};
         let find_options = FindOptions::builder().sort(doc!{"date_stamp":1}).build();
         let cursor = collection.find(filter, find_options).unwrap();
         let mut res = Vec::new();
@@ -51,7 +60,7 @@ impl QAMongoClient {
         let collection = self.database.collection("stock_day");
 
         let filter = doc! {"code": code,
-                                            "date": {"$gte": start, "$lte": end}};
+                                            "date_stamp": {"$gte": to_timestamp(start.to_string()), "$lte":  to_timestamp(end.to_string())}};
         let find_options = FindOptions::builder().sort(doc!{"date_stamp":1}).build();
         let cursor = collection.find(filter, find_options).unwrap();
         let mut res = Vec::new();
@@ -150,3 +159,14 @@ impl QAMongoClient {
     // }
 }
 
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_totimestamp(){
+        println!("{:#?}",to_timestamp("2020-01-01".to_string()));
+        //assert_eq!(1577836800)
+    }
+}
