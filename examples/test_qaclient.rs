e62959f499ba412076ccfd5be68b2eea36108443
yutiansut <yutiansut@qq.com>
2020-12-26 18:11:36 +0800

    #update for new qadata

--- examples/test_qaclient.rs
+++ examples/test_qaclient.rs
@@ -1,6 +1,22 @@
-use qadata_rs;
+use qadata_rs::qafetch::QAMongoClient;
 
+use mifi_rs::kline::stock_min;
 
 fn main(){
+    let qamc= QAMongoClient::new("mongodb://192.168.2.117", "quantaxis");
 
+
+    let mut dataclient = QAMongoClient::new("mongodb://192.168.2.118:27017", "quantaxis");
+    let stock_day = dataclient.get_stock_day("000001", "2020-01-01", "2020-02-01");
+    // println!("{:#?}", stock_day);
+    // let stock_min = dataclient.get_stock_min("000001", "2020-01-01", "2020-02-01", "5min");
+    // println!("{:#?}", stock_min);
+    //
+    //
+    // let future_day = dataclient.get_stock_day("RBL8", "2020-01-01", "2020-02-01");
+    // println!("{:#?}", future_day);
+
+
+    let future_min = dataclient.get_future_min("RBL8", "2020-06-01", "2020-07-01", "1min");
+    println!("{:#?}", future_min);
 }
